17aaae5c2fe6ad5d64b5c57500593562bd146168
marcelns <marcelonascimentossilva@gmail.com>
2021-11-13 13:10:25 -0300

    First model
    First linear regression model. Next: select the least square variables.

--- exercises/r-regression-models.R
+++ exercises/r-regression-models.R
@@ -12,3 +12,12 @@ library("fpp2")
 # US consumption expenditure  quarterly percentage changes
 autoplot(uschange[,c("Consumption","Income")]) +
   ylab("% change") + xlab("Year")
+
+View(uschange)
+
+fit.consMR <- tslm(
+  Consumption ~ Income + Production + Savings + Unemployment,
+  data=uschange)
+summary(fit.consMR)
+
+0.26729/0.03721
